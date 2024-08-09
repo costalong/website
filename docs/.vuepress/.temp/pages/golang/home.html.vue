ca370f0bc562a9a511655e7962d55f7f23e9568e
csotalong <costa9293@gmail.com>
2024-08-09 17:17:21 +0800

    feat: 修改链接

--- docs/.vuepress/.temp/pages/golang/home.html.vue
+++
@@ -1,5 +0,0 @@
-<template><div><h2 id="golang-的介绍" tabindex="-1"><a class="header-anchor" href="#golang-的介绍"><span>golang 的介绍</span></a></h2>
-<p>golang 是强类型的语言</p>
-</div></template>
-
-
--- docs/.vuepress/config/sidebar/zh/k8s.ts
+++ docs/.vuepress/config/sidebar/zh/k8s.ts
@@ -15,7 +15,7 @@ export const k8s = arraySidebar([
                 collapsible: true,
                 children: [
                     "ubuntu-install",
-                    "ubuntu-error",
+                    "install-error",
                 ],
             },
             {
