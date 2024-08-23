93131d53fabe2c312c2b7d6fe5fb623b30b083bf
csotalong <costa9293@gmail.com>
2024-08-23 11:07:14 +0800

    fix: 修改代码

--- docs/.vuepress/config/sidebar/zh/golang.ts
+++ docs/.vuepress/config/sidebar/zh/golang.ts
@@ -1,57 +1,58 @@
 import { arraySidebar } from "vuepress-theme-hope";
 export const golang = arraySidebar(
-    [
+  [
+    {
+      text: "golang的文档",
+      icon: "golang2",
+      prefix: '/golang/',
+      collapsible: true,
+      children: [
+        'home',
         {
-            text: "golang的文档",
-            icon: "golang2",
-            prefix: '/golang/',
-            collapsible: true,
-            children: [
-                'home',
-                {
-                    text: "监控",
-                    prefix: 'performance/',
-                    icon: "performance",
-                    collapsible: true,
-                    link: "performance/",
-                    children: [
-                        "sentry",
-                    ],
-                },
-                {
-                    text: "go 常用的包",
-                    prefix: 'packages/',
-                    icon: "Package",
-                    collapsible: true,
-                    link: "packages/",
-                    children: [
-                        "slices",
-                    ],
-                },
-                {
-                    text: "测试",
-                    prefix: 'tests/',
-                    icon: "test",
-                    collapsible: true,
-                    link: "tests/",
-                    children: [
-                        "gomonkey",
-                        "gomock",
-                        "goconvey",
-                    ],
-                },
-                {
-                    text: "工具",
-                    prefix: 'tools/',
-                    icon: "tools",
-                    collapsible: true,
-                    link: "tools/",
-                    children: [
-                        "gvm",
-                    ],
-                },
-            ],
-        }
-    ]
+          text: "监控",
+          prefix: 'performance/',
+          icon: "performance",
+          collapsible: true,
+          link: "performance/",
+          children: [
+            "sentry",
+          ],
+        },
+        {
+          text: "go 常用的包",
+          prefix: 'packages/',
+          icon: "Package",
+          collapsible: true,
+          link: "packages/",
+          children: [
+            "slices",
+            "bufio",
+          ],
+        },
+        {
+          text: "测试",
+          prefix: 'tests/',
+          icon: "test",
+          collapsible: true,
+          link: "tests/",
+          children: [
+            "gomonkey",
+            "gomock",
+            "goconvey",
+          ],
+        },
+        {
+          text: "工具",
+          prefix: 'tools/',
+          icon: "tools",
+          collapsible: true,
+          link: "tools/",
+          children: [
+            "gvm",
+          ],
+        },
+      ],
+    }
+  ]
 )
 
